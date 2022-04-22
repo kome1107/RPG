591f7d69252f66f207f222a3eb0837dbae62d59d
Ssaaz <101115828+kome1107@users.noreply.github.com>
2022-04-22 20:55:02 +0900

    再課題提出

--- src/practic/Human.java
+++ src/practic/Human.java
@@ -2,7 +2,4 @@ package practic;
 
 public abstract class Human extends Living {
 
-
-	abstract void attack(Living target); // ターゲットに攻撃するメソッド
-
 }
--- src/practic/Monster.java
+++ src/practic/Monster.java
@@ -1,9 +1,5 @@
 package practic;
 
 public abstract class Monster extends Living {
-	
-
-	abstract void attack(Living target); // ターゲットに攻撃するメソッド
-
 
 }
